4da8da4163907d6cd3a044523e7ecd9782ff2ed7
Christian von Krogh Evensen <584988@stud.hvl.no>
2022-05-11 23:12:04 +0200

    added grafikk

---
+++ C#/Graphics/Grafikk/Grafikk/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafikk.Models
+{
+    public class Car
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Car(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void MoveE()
+        {
+            x += 10;
+        }
+        public void MoveN()
+        {
+            x += 10;
+        }
+
+       
+    }
+}
