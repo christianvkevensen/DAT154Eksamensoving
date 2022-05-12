3cb7367f0cff25ab153c726363c81bde911fd797
Christian Evensen <h584988@stud.hvl.no>
2022-05-12 16:43:20 +0200

    delegatesmethod

---
+++ C#/Delegate/DelegateMethod/DelegateMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DelegateMethod
+{
+    public class Program
+    {
+        public static bool isEqual(int x)
+        {
+            if(x % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool isOdd(int x)
+        {
+            if(x % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public delegate bool DelegateBool(int x);
+
+        public static void Test(int x, DelegateBool del)
+        {
+            Console.WriteLine(del(x));
+        }
+        public static void Test2(int x, Func<int,bool> del)
+        {
+            Console.WriteLine(del(x));
+        }
+        public static void Main()
+        {
+            Test(3, isEqual);
+            Test2(3, isOdd);
+        }
+    }
+}
